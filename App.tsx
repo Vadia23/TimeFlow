6fd005a4f84547f948ff43c6914f9cd605d35476
BVF <BVF@d.c>
2023-06-07 15:28:27 +0300

    basic project setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+	return (
+		<View className=' bg-blue-600'>
+			<Text>Open 23</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
+}
