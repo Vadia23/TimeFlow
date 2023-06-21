52bf49a282a5ea5d758b25d60739396a0edbdb2a
BVF <BVF@d.c>
2023-06-21 18:11:05 +0300

    create authPage

---
+++ app/components/ui/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import { FC, PropsWithChildren } from 'react'
+import { Platform, SafeAreaView, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+const Layout: FC<PropsWithChildren<{ title?: string }>> = ({
+	children,
+	title
+}) => {
+	const { top } = useSafeAreaInsets()
+
+	return (
+		<SafeAreaView className='flex-1'>
+			<View
+				className='flex-1 px-6'
+				style={{
+					padding: Platform.OS === 'ios' ? top / 5 : top * 1.6
+				}}
+			>
+				{title && (
+					<Text className='text-white text-4xl font-semibold text-center'>
+						{title}
+					</Text>
+				)}
+				<View>{children}</View>
+			</View>
+		</SafeAreaView>
+	)
+}
+
+export default Layout
--- app/providers/AuthProv.tsx
+++ app/providers/AuthProv.tsx
@@ -25,7 +25,7 @@ export const AuthContext = createContext({} as IContext)
 let ignore = Splash.preventAutoHideAsync()
 
 const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
-	const [user, setUser] = useState<TypeUserState>({} as IUser)
+	const [user, setUser] = useState<TypeUserState>(null)
 
 	console.log(user)
 	useEffect(() => {
